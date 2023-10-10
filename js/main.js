b2ec2e16f0540960921bd6fb1dd5ccf6a9f826f7
gitazeinstra <gitazeinstra@gmail.com>
2023-10-10 21:17:46 +0200

    scroll up button

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ AOS.init({
 
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
+    const scrollBtn = document.querySelector('.scroll-up')
     const scrollTop = window.scrollY;
 
     if (scrollTop > 60) { // You can adjust this value to determine when the navbar becomes sticky
         navbar.classList.add('solid');
+        scrollBtn.style.display = "block";
     } else {
         navbar.classList.remove('solid');
+        scrollBtn.style.display = "none";
     }
 });
 
