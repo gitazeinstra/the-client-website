84b7f8f20bc65493c7c9963ce94a24b8f54d9a41
gitazeinstra <gitazeinstra@gmail.com>
2023-10-11 12:01:55 +0200

    Verbeteringen en toevoegingen
    Semantische code verbeterd
    mobile navbar focudbaar gemaakt
    Aantal secties responsive voor een klein laptop scherm

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ AOS.init({
     duration: 1200,
 })
 
+const navbar = document.querySelector('.navbar');
+const scrollBtn = document.querySelector('.scroll-up')
+
 window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    const scrollBtn = document.querySelector('.scroll-up')
     const scrollTop = window.scrollY;
 
     if (scrollTop > 60) { // You can adjust this value to determine when the navbar becomes sticky
